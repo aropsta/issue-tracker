07e3300cf125c2548299df6ec3756ce9e36bd0c6
aropsta <aropsta16@gmail.com>
2024-09-09 11:54:44 +1000

    setup prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+
+import classnames from "classnames";
 
 const NavBar = () => {
   const links = [
@@ -13,6 +17,7 @@ const NavBar = () => {
       href: "/issues",
     },
   ];
+  const currentPath = usePathname();
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -20,11 +25,17 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <li
-            key={link.href}
-            className="text0zinc-500 hover:text-zinc-800 transition-colors"
-          >
-            <Link href={link.href}>{link.label}</Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
           </li>
         ))}
       </ul>
