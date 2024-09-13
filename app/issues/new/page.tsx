38ebc1e7d2c7a1de4a7b500cc8e99ea1f13b918d
aropsta <aropsta16@gmail.com>
2024-09-13 15:48:45 +1000

    UI callout component when theres an error submiting the form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,20 @@
 "use client";
-import { Button, Flex, TextArea, TextField } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Callout,
+  Flex,
+  TextArea,
+  TextField,
+} from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller, Form } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { RiAlertFill } from "react-icons/ri";
+import { RiAB } from "react-icons/ri";
 
 interface IssueForm {
   title: string;
@@ -18,35 +28,54 @@ const NewIssuePage = () => {
   //nextjs router
   const router = useRouter();
 
+  const [error, setError] = useState(false);
+
   //function to submit form data to server
   async function submitRequest(data: IssueForm) {
-    await axios.post("/api/issues", data);
-    //redirect user back to issues page
-    router.push("/issues");
+    try {
+      await axios.post("/api/issues", data);
+      //redirect user back to issues page
+      router.push("/issues");
+    } catch (error) {
+      setError(true);
+    }
   }
 
   return (
-    <form
-      className="space-y-3 max-w-xl"
-      onSubmit={handleSubmit((data) => submitRequest(data))}
-    >
-      <TextField.Root
-        placeholder="Title"
-        className=""
-        {...register("title")}
-      ></TextField.Root>
+    <Box className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-5">
+          <Callout.Icon>
+            <RiAlertFill />
+          </Callout.Icon>
+          <Callout.Text>
+            There was an error submiting your request.
+          </Callout.Text>
+        </Callout.Root>
+      )}
 
-      {/* Must wrap simpleMDE in a controller from react hook form as simpleMDE component doesnt support additional props with spread operator */}
-      <Controller
-        name="description"
-        control={control}
-        // TODO: Customize markdown editor */}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" className="" {...field} />
-        )}
-      />
-      <Button className="self-start">Submit New Issue</Button>
-    </form>
+      <form
+        onSubmit={handleSubmit((data) => submitRequest(data))}
+        className="space-y-3"
+      >
+        <TextField.Root
+          placeholder="Title"
+          className=""
+          {...register("title")}
+        ></TextField.Root>
+
+        {/* Must wrap simpleMDE in a controller from react hook form as simpleMDE component doesnt support additional props with spread operator */}
+        <Controller
+          name="description"
+          control={control}
+          // TODO: Customize markdown editor */}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" className="" {...field} />
+          )}
+        />
+        <Button className="self-start">Submit New Issue</Button>
+      </form>
+    </Box>
   );
 };
 
