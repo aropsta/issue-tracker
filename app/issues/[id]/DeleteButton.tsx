defb08b4c1fe75f832e86a9bc070022fd534bfce
aropsta <aropsta16@gmail.com>
2024-09-14 15:15:07 +1000

    alert dialog for deleting an issue

--- app/issues/[id]/DeleteButton.tsx
+++ app/issues/[id]/DeleteButton.tsx
@@ -1,11 +1,35 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 interface Props {
   issueId: number;
 }
 const DeleteButton = ({ issueId }: Props) => {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to <strong>permanantly</strong> delete this
+          issue?
+        </AlertDialog.Description>
+        <Flex gap="4" align="center" mt="4" justify="end">
+          <AlertDialog.Action>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Action>
+          <AlertDialog.Cancel>
+            <Button variant="surface" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteButton;
