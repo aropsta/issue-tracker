72fbbb85cd2401aa075fc53ff1db32d176a66c86
aropsta <aropsta16@gmail.com>
2024-09-14 17:13:43 +1000

    filter componenet

--- app/issues/list/IssuesHeader.tsx
+++ app/issues/list/IssuesHeader.tsx
@@ -1,10 +1,12 @@
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import StatusFilter from "./StatusFilter";
 
 const IssuesHeader = () => {
   return (
-    <Flex className="mb-4">
+    <Flex mb="4" justify="between">
+      <StatusFilter />
       <Button>
         <Link href="/issues/new" prefetch={true}>
           New issue
