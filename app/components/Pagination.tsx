30a89cab9515fa3c8efc927844c372ba21f7d5aa
aropsta <aropsta16@gmail.com>
2024-09-14 21:54:16 +1000

    refactored pagination to use search query params and not usestate()

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -6,6 +6,7 @@ import {
   DoubleArrowRightIcon,
 } from "@radix-ui/react-icons";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
 interface Props {
@@ -14,31 +15,60 @@ interface Props {
   currentPage: number;
 }
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
-  const [page, setPage] = useState(currentPage);
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
+  function changePage(page: number) {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  }
+
   return (
     <Flex gap="4">
       {/* first */}
-      <Button disabled={page === 1} onClick={() => setPage(1)}>
+      <Button
+        disabled={currentPage === 1}
+        onClick={() => {
+          changePage(1);
+          console.log("current Page: ", currentPage);
+        }}
+      >
         <DoubleArrowLeftIcon />
       </Button>
       {/* prev */}
-      <Button disabled={page === 1} onClick={() => setPage((prev) => --prev)}>
+      <Button
+        disabled={currentPage === 1}
+        onClick={() => {
+          console.log("current Page: ", currentPage);
+          changePage(currentPage - 1);
+        }}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Text>
-        Page: {page} of {pageCount}
+      <Text size="2">
+        Page {currentPage} of {pageCount}
       </Text>
       {/* next */}
       <Button
-        disabled={page === pageCount}
-        onClick={() => setPage((prev) => ++prev)}
+        disabled={currentPage === pageCount}
+        onClick={() => {
+          console.log("current Page: ", currentPage);
+          changePage(currentPage + 1);
+        }}
       >
         <ChevronRightIcon />
       </Button>
       {/* last */}
-      <Button disabled={page === pageCount} onClick={() => setPage(pageCount)}>
+      <Button
+        disabled={currentPage === pageCount}
+        onClick={() => {
+          console.log("current Page: ", currentPage);
+          changePage(pageCount);
+        }}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Pagination from "./components/Pagination";
 
-const page = () => {
-  return <Pagination itemCount={100} pageSize={9} currentPage={2} />;
+const page = ({ searchParams }: { searchParams: { page: string } }) => {
+  return (
+    <Pagination
+      itemCount={100}
+      pageSize={9}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 };
 
 export default page;
