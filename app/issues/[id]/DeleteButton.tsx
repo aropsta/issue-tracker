58db9c5bbd7ab64748203fd5666647ec603ffef3
aropsta <aropsta16@gmail.com>
2024-09-14 15:52:20 +1000

    axios error handling for deleting

--- app/issues/[id]/DeleteButton.tsx
+++ app/issues/[id]/DeleteButton.tsx
@@ -2,45 +2,65 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   issueId: number;
 }
 const DeleteButton = ({ issueId }: Props) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  async function onDelete() {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (err) {
+      setError(true);
+    }
+  }
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to <strong>permanantly</strong> delete this
-          issue?
-        </AlertDialog.Description>
-        <Flex gap="4" align="center" mt="4" justify="end">
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={() => {
-                axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
-            >
-              Delete
-            </Button>
-          </AlertDialog.Action>
-          <AlertDialog.Cancel>
-            <Button variant="surface" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to <strong>permanantly</strong> delete this
+            issue?
+          </AlertDialog.Description>
+          <Flex gap="3" align="center" mt="4" justify="end">
+            <AlertDialog.Action>
+              <Button color="red" onClick={async () => onDelete()}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+            <AlertDialog.Cancel>
+              <Button variant="surface" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>Delete Failed</AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            mt="4"
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
