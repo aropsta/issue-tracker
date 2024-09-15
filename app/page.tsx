bd631a63c0195e9f9f6fd16f765c0690afd52bc9
aropsta <aropsta16@gmail.com>
2024-09-15 19:16:37 +1000

    Created summary componenet

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import Summary from "./Summary";
+import prisma from "@/prisma/client";
 
-const page = ({ searchParams }: { searchParams: { page: string } }) => {
-  return <LatestIssues />;
+const page = async ({ searchParams }: { searchParams: { page: string } }) => {
+  const openCount = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closedCount = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const progressCount = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <>
+      <Summary
+        open={openCount}
+        closed={closedCount}
+        inProgress={progressCount}
+      />
+      <LatestIssues />
+    </>
+  );
 };
 
 export default page;
