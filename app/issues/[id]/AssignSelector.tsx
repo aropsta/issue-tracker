3da22a64dd7ea0b1cccdcf8cb9927ad4e4782a91
aropsta <aropsta16@gmail.com>
2024-09-16 21:06:58 +1000

    api call in user selector component to fetch list of users

--- app/issues/[id]/AssignSelector.tsx
+++ app/issues/[id]/AssignSelector.tsx
@@ -1,14 +1,31 @@
+"use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssignSelector = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign to..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Users</Select.Label>
-          <Select.Item value="1">Robba Dengs</Select.Item>
+          {users.map((u) => (
+            <Select.Item key={u.id} value={u.id}>
+              {u.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
