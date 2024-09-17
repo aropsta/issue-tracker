f3d57a165cac74d127f4b99aad144324780622bb
aropsta <aropsta16@gmail.com>
2024-09-17 19:48:37 +1000

    typos fixed

--- prisma/migrations/20240916112603_assign_users_issues/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Issue` ADD COLUMN `assignedToUerId` VARCHAR(255) NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedToUerId_fkey` FOREIGN KEY (`assignedToUerId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
